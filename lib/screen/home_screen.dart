c51c5b2e47d4be270b4a1a6fd3ab7ced47afb7b3
sinasystem <sinasys>
2022-03-18 19:34:35 +0330

    Remove unused import files

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:nav_rail_bottom_nav_reponsive/responsive_util.dart';
-import 'package:nav_rail_bottom_nav_reponsive/screen/profile_screen.dart';
-import 'package:nav_rail_bottom_nav_reponsive/screen/favorite_screen.dart';
-import 'package:nav_rail_bottom_nav_reponsive/screen/setting_screen.dart';
 import 'package:nav_rail_bottom_nav_reponsive/widgets/bottom_nav.dart';
 import 'package:nav_rail_bottom_nav_reponsive/widgets/nav_rail.dart';
 import 'package:nav_rail_bottom_nav_reponsive/widgets/tab_bar.dart';
